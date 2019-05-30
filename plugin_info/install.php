edcfb4310da9b449fc7143e8644a3c334cf7fc19
Florentin Munsch <Florentin68@users.noreply.github.com>
2019-05-30 19:59:49 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -18,16 +18,16 @@
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
-function template_install() {
+function todoist_install() {
     
 }
 
-function template_update() {
+function todoist_update() {
     
 }
 
 
-function template_remove() {
+function todoist_remove() {
     
 }
 
